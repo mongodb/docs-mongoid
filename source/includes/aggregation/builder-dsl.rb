eff4e1daae733f1159ab1555a6e1cfffea98a3cc
Jordan Smith <jordan.smith@mongodb.com>
2024-10-28 09:14:25 -0700

    first pass aggregation

---
+++ source/includes/aggregation/builder-dsl.rb
@@ -0,0 +1,6 @@
+criteria = Tour.where('participants.name' => 'Serenity').
+   unwind(:states).
+   group(_id: 'states', :states.add_to_set => '$states').
+   project(_id: 0, states: 1)
+
+Tour.collection.aggregate(criteria.pipeline).to_a
