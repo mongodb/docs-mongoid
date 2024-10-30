420c82f12570cb4bdc6afbd84debce331aa94fc1
Jordan Smith <jordan.smith@mongodb.com>
2024-10-30 10:12:18 -0700

    feedback round 2

--- source/includes/aggregation/builder-dsl.rb
+++ source/includes/aggregation/builder-dsl.rb
@@ -1,6 +1,6 @@
-criteria = Tour.where('participants.name' => 'Serenity').
-   unwind(:states).
-   group(_id: 'states', :states.add_to_set => '$states').
-   project(_id: 0, states: 1)
+criteria = Tour.where('participant.name' => 'Serenity').
+    unwind(:states).
+    group(_id: 'states', :states.add_to_set => '$states').
+    project(_id: 0, states: 1)
 
-Tour.collection.aggregate(criteria.pipeline).to_json
+@states = Tour.collection.aggregate(criteria.pipeline).to_json
