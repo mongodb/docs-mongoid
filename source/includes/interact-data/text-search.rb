4ffdfac699ec6f21556cda78de6793a42cff811a
rustagir <rea.rustagi@mongodb.com>
2024-11-22 11:53:44 -0500

    wip

--- source/includes/interact-data/text-search.rb
+++ source/includes/interact-data/text-search.rb
@@ -8,3 +8,15 @@ class Dish
   index description: 'text'
 end
 # end-text-index-model
+
+# start-term
+Dish.where('$text' => {'$search' => 'herb'})
+# end-term
+
+# start-phrase
+Dish.where('$text' => {'$search' => "\"serves 2\""})
+# end-phrase
+
+# start-exclude
+Dish.where('$text' => {'$search' => 'vegan -tofu'})
+# end-exclude
