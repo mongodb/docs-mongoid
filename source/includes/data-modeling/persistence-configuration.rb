39a9ac9fcd1beb582e183463ea8f92213e6c12c9
Maya Raman <maya.raman19@gmail.com>
2024-12-18 09:59:28 -0800

    code space

--- source/includes/data-modeling/persistence-configuration.rb
+++ source/includes/data-modeling/persistence-configuration.rb
@@ -1,16 +1,16 @@
 # start default modeling
 class Restaurant
-    include Mongoid::Document
+  include Mongoid::Document
 end
 
 class Person
-    include Mongoid::Document
+  include Mongoid::Document
 end
 # end default modeling
 
 # start set pluralization
 ActiveSupport::Inflector.inflections do |inflect|
-    inflect.plural("rey", "reyes")
+  inflect.plural("rey", "reyes")
 end
 # end set pluralization
 
@@ -34,15 +34,15 @@ end
 
 # start store_in example
 class Person
-    include Mongoid::Document
-    store_in collection: "citizens", database: "other", client: "analytics"
+  include Mongoid::Document
+  store_in collection: "citizens", database: "other", client: "analytics"
 end
 # end store_in example
 
 # start store_in lambda example
 class Band
-    include Mongoid::Document
-    store_in database: ->{ Thread.current[:database] }
+  include Mongoid::Document
+  store_in database: ->{ Thread.current[:database] }
 end
 # end store_in lambda example
 
@@ -65,50 +65,50 @@ end
 # Creates document in 'bands' collection in 'music-non-stop' database within
 # default cluster
 Band.with(database: "music-non-stop") do |band_class|
-    band_class.create(name: "Medusa and the Polyps")
+  band_class.create(name: "Medusa and the Polyps")
 end
 
 # Deletes all documents in 'artists' collection within default database
 Band.with(collection: "artists") do |band_class|
-    band_class.delete_all
+  band_class.delete_all
 end
 
 band = Band.new(name: "Japanese Breakfast")
 
 # Perform operations on tertiary cluster
 band.with(client: :tertiary) do |band_object|
-    band_object.save!
+  band_object.save!
     
-    band.save!
+  band.save!
 end
 # end with example
 
 # start read configuration
 Band.with(read: {mode: :secondary}) do
-    Band.count
+  Band.count
 
-    # Write operation runs in new
-    # persistence context, but is not
-    # affected by the read preference.
-    Band.create(name: "Metallica")
+  # Write operation runs in new
+  # persistence context, but is not
+  # affected by the read preference.
+  Band.create(name: "Metallica")
 end
 # end read configuration
 
 # start global configuration example
 class BandsController < ApplicationController
-    before_action :switch_database
-    after_action :reset_database
+  before_action :switch_database
+  after_action :reset_database
 
-    private
+  private
 
-    def switch_database
-      I18n.locale = params[:locale] || I18n.default_locale
-      Mongoid.override_database("my_db_name_#{I18n.locale}")
-    end
+  def switch_database
+    I18n.locale = params[:locale] || I18n.default_locale
+    Mongoid.override_database("my_db_name_#{I18n.locale}")
+  end
 
-    def reset_database
-      Mongoid.override_database(nil)
-    end
+  def reset_database
+    Mongoid.override_database(nil)
+  end
 end
 # end global configuration example
 
@@ -122,12 +122,12 @@ band.collection
 
 # start client with example
 Band.mongo_client.with(write: { w: 0 }, database: "music") do |client|
-    client[:artists].find(...)
+  client[:artists].find(...)
 end
 # end client with example
 
 # start collection with example
 Band.collection.with(write: { w: 0 }) do |collection|
-    collection[:artists].find(...)
+  collection[:artists].find(...)
 end
 # end collection with example
